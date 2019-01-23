c3cbf8d6434598cade086c33a86f679cd5de7ec2
abhinavdhasmana <abhinav.dhasmana@live.com>
2019-01-23 17:40:49 +0530

    convert input into frames when there is no spare and no strike

--- evalauationOne.js
+++ evalauationOne.js
@@ -1,4 +1,12 @@
 const validateAllThrows = allThrows => allThrows.every(val => ((val >= 0) && (val <= 10)));
 
+const convertInputIntoFrames = (allThrows) => {
+  const allFrames = [];
+  while (allThrows.length > 0) {
+    allFrames.push(allThrows.splice(0, 2));
+  }
+  return allFrames;
+};
 
-module.exports = { validateAllThrows };
+
+module.exports = { validateAllThrows, convertInputIntoFrames };
--- evaluationOne.test.js
+++ evaluationOne.test.js
@@ -1,4 +1,4 @@
-const { validateAllThrows } = require('./evalauationOne');
+const { validateAllThrows, convertInputIntoFrames } = require('./evalauationOne');
 
 describe('validateAllThrows', () => {
   it('should return false if any value is < 0', () => {
@@ -9,5 +9,22 @@ describe('validateAllThrows', () => {
   });
   it('should return true if all values are in the range of 0 and 10 including', () => {
     expect(validateAllThrows([0, 5, 10, 7])).toEqual(true);
+  });
+});
+
+describe('convertInputIntoFrames', () => {
+  it('should convert no strike, no spare input into 10 frames', () => {
+    const input = [1, 2, 3, 4, 5, 6, 7, 8, 9, 8, 7, 6, 5, 4, 3, 2, 1, 0, 1, 2];
+    const allFrames = convertInputIntoFrames(input);
+    expect(allFrames.length).toEqual(10);
+  });
+
+  it('should convert no strike, no spare input into frames in the same order', () => {
+    const input = [1, 2, 3, 4, 5, 6, 7, 8, 9, 8, 7, 6, 5, 4, 3, 2, 1, 0, 1, 2];
+    const expectedOutput = [
+      [1, 2], [3, 4], [5, 6], [7, 8], [9, 8], [7, 6], [5, 4], [3, 2], [1, 0], [1, 2],
+    ];
+    const allFrames = convertInputIntoFrames(input);
+    expect(allFrames).toEqual(expectedOutput);
   });
 });
