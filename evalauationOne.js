606fa3671baaaad62367f73e58bde1f1bb08ee43
abhinavdhasmana <abhinav.dhasmana@live.com>
2019-01-24 15:27:15 +0530

    calculate score working for all valid inputs

--- evalauationOne.js
+++ evalauationOne.js
@@ -5,6 +5,8 @@ const convertInputIntoFrames = (allThrows) => {
   while (allThrows.length > 0) {
     if (allThrows.length === 3) {
       allFrames.push(allThrows.splice(0, 3));
+    } else if (allThrows[0] === 10) {
+      allFrames.push(allThrows.splice(0, 1));
     } else {
       allFrames.push(allThrows.splice(0, 2));
     }
@@ -12,5 +14,37 @@ const convertInputIntoFrames = (allThrows) => {
   return allFrames;
 };
 
+const findNextTwoValues = (index, allFrames) => {
+  if (allFrames[index].length === 1) {
+    return allFrames[index][0] + allFrames[index + 1][0];
+  }
+  // if (allFrames[index].length === 2) {
+  return allFrames[index][0] + allFrames[index][1];
+  // }
+};
 
-module.exports = { validateAllThrows, convertInputIntoFrames };
+const calculateScoreFromFrames = (allFrames) => {
+  let sum = 0;
+  for (let i = 0; i < allFrames.length - 1; i += 1) {
+    if (allFrames[i].length === 1) {
+      sum += findNextTwoValues(i + 1, allFrames) + 10;
+    } else if ((allFrames[i][0] + allFrames[i][1]) === 10) {
+      sum += (allFrames[i][0] + allFrames[i][1]) + allFrames[i + 1][0];
+    } else {
+      sum += (allFrames[i][0] + allFrames[i][1]);
+    }
+  }
+  const tenthFrameTotal = allFrames[9].reduce((acc, val) => acc + val, 0);
+  return sum + tenthFrameTotal;
+};
+
+const score = (allThrows) => {
+  if (validateAllThrows(allThrows)) {
+    const frames = convertInputIntoFrames(allThrows);
+    return calculateScoreFromFrames(frames);
+  }
+  return 'invalid input';
+};
+
+
+module.exports = { validateAllThrows, convertInputIntoFrames, score };
--- evaluationOne.test.js
+++ evaluationOne.test.js
@@ -1,4 +1,4 @@
-const { validateAllThrows, convertInputIntoFrames } = require('./evalauationOne');
+const { validateAllThrows, convertInputIntoFrames, score } = require('./evalauationOne');
 
 describe('validateAllThrows', () => {
   it('should return false if any value is < 0', () => {
@@ -53,5 +53,97 @@ describe('convertInputIntoFrames', () => {
     ];
     const allFrames = convertInputIntoFrames(input);
     expect(allFrames).toEqual(expectedOutput);
+  });
+
+  it('should convert single strike input into frames of length 10', () => {
+    const input = [10, 7, 2, 4, 5, 6, 1, 8, 1, 8, 1, 6, 2, 4, 3, 7, 1, 0, 5];
+    // const expectedOutput = [
+    //   [1, 9], [3, 4], [5, 6], [7, 8], [9, 8], [7, 6], [5, 4], [3, 7], [1, 0], [5, 5, 3],
+    // ];
+    const allFrames = convertInputIntoFrames(input);
+    expect(allFrames.length).toEqual(10);
+  });
+
+  it('should convert multiple strike input into frames of length 10', () => {
+    const input = [10, 7, 1, 10, 5, 6, 1, 1, 10, 8, 1, 6, 2, 4, 3, 7, 1];
+    // const expectedOutput = [
+    //   [1, 9], [3, 4], [5, 6], [7, 8], [9, 8], [7, 6], [5, 4], [3, 7], [1, 0], [5, 5, 3],
+    // ];
+    const allFrames = convertInputIntoFrames(input);
+    expect(allFrames.length).toEqual(10);
+  });
+});
+
+describe('score', () => {
+  describe('Check game can be scored correctly.', () => {
+    it('should be able to score a game with all gutterballs', () => {
+      const rolls = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
+      expect(score(rolls)).toEqual(0);
+    });
+
+    it('should be able to score a game with no strikes or spares', () => {
+      const rolls = [3, 6, 3, 6, 3, 6, 3, 6, 3, 6, 3, 6, 3, 6, 3, 6, 3, 6, 3, 6];
+      expect(score(rolls)).toEqual(90);
+    });
+
+    it('a spare followed by zeros is worth ten points', () => {
+      const rolls = [6, 4, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
+      expect(score(rolls)).toEqual(10);
+    });
+
+    it('points scored in the roll after a spare are counted twice', () => {
+      const rolls = [6, 4, 3, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
+      expect(score(rolls)).toEqual(16);
+    });
+
+    it('consecutive spares each get a one-roll bonus', () => {
+      const rolls = [5, 5, 3, 7, 4, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
+      expect(score(rolls)).toEqual(31);
+    });
+
+    it('should allow fill ball the last frame is a spare', () => {
+      const rolls = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 7, 3, 7];
+      expect(score(rolls)).toEqual(17);
+    });
+
+    it('a strike earns ten  points in a frame with a single roll', () => {
+      const rolls = [10, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
+      expect(score(rolls)).toEqual(10);
+    });
+
+    it('points scored in the two rolls after a strike are counted twice as a bonus', () => {
+      const rolls = [10, 5, 3, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
+      expect(score(rolls)).toEqual(26);
+    });
+
+    it('should be able to score multiple strikes in a row', () => {
+      const rolls = [10, 10, 10, 5, 3, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
+      expect(score(rolls)).toEqual(81);
+    });
+
+    it('should allow fill balls when the last frame is a strike', () => {
+      const rolls = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 10, 7, 1];
+      expect(score(rolls)).toEqual(18);
+    });
+
+    it('rolling a spare with the two-roll bonus does not get a bonus roll', () => {
+      const rolls = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 10, 7, 3];
+      expect(score(rolls)).toEqual(20);
+    });
+
+    it('strikes with the two-roll bonus do not get bonus rolls', () => {
+      const rolls = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 10, 10, 10];
+      expect(score(rolls)).toEqual(30);
+    });
+
+    it('a strike with the one-roll bonus after a spare in the last frame does not get a bonus', () => {
+      const rolls = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 7, 3, 10];
+      expect(score(rolls)).toEqual(20);
+    });
+
+    it('should be able to score a perfect game', () => {
+      const rolls = [10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10];
+      expect(score(rolls)).toEqual(300);
+    });
   });
 });
